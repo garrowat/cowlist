9bbe85f0ed0d83798d2dba9ce66aa352670c2c0e
Garrett Watson <garrettbauer@gmail.com>
2019-06-24 23:25:13 +0900

    Completes POST route for /api/cows, databases successfully populates and returns result

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require('express');
+const parser = require('body-parser');
 const app = express();
 const db = require('./db');
 const models = require('./models');
 const port = 3000;
+
+app.use(parser.json());
 
 app.get('/', (req, res) => {
   res.send('MMMMmmooOOoooooOOooooOOOOOooOOooo');
 });
 
 app.post('/api/cows', (req, res) => {
-  models.set( (err, result) => {
+  var params = [req.body.name, req.body.description];
+  models.set( params, (err, result) => {
     if (err) {
       res.status(400).send(`ERROR: MMMOOooooooooo, ${err}`);
     } else {
-      res.json(result);
+      res.json(req.body);
     }
   });
 });
--- models/index.js
+++ models/index.js
@@ -1,13 +1,19 @@
 var db = require('../db');
 
 module.exports = {
-  set: function(callback) {
+  set: function(params, callback) {
     console.log(`processing SET request to DB`);
+
+    let queryString = 'INSERT INTO cows (name, description) VALUES (?, ?)';
+
+    db.query(queryString, params, (err, results) => {
+      callback(err, results);
+    });
   },
   get: function(callback) {
     console.log(`processing GET request to DB`);
 
-    let queryString = 'SELECT name, description FROM cows;';
+    let queryString = 'SELECT name, description FROM cows';
 
     db.query(queryString, (err, results) => {
       callback(err, results);
